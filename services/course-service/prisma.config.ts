9c9b605814355bbb261d47d80ef917960611be94
Lê Quang Vinh <105306823+qvinh0402@users.noreply.github.com>
2026-04-13 21:37:20 +0700

    update src

--- services/course-service/prisma.config.ts
+++ services/course-service/prisma.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from '@prisma/config'
+import * as dotenv from 'dotenv'
+
+// Nạp các biến từ file .env vào process.env
+dotenv.config()
 
 export default defineConfig({
   datasource: {
-    // Prisma 7 sẽ lấy URL từ biến môi trường thông qua cấu hình này
+    // Bây giờ process.env.DATABASE_URL sẽ có giá trị
     url: process.env.DATABASE_URL,
   },
 })
